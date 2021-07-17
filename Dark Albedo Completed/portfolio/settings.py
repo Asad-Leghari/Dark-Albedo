f20a6f256d0661c7a5357d1ff1a80b1d86acf2d0
Asad Leghari <72187799+Asad-Leghari@users.noreply.github.com>
2021-07-16 19:10:38 -0700

    updated to make it public
    removed sensitive email login info from settings.py

--- Dark Albedo Completed/portfolio/settings.py
+++ Dark Albedo Completed/portfolio/settings.py
@@ -139,8 +139,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST= 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'asadleghari537@gmail.com'
-EMAIL_HOST_PASSWORD = 'cbkaygptsvfocqkb'
+EMAIL_HOST_USER = 'input your email'
+EMAIL_HOST_PASSWORD = 'input your password'
 ################
 
 # Default primary key field type
