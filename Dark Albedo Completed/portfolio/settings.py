6e46ae09a379dc0dae471b61a6133fdcc8030c3e
Asad Leghari <72187799+Asad-Leghari@users.noreply.github.com>
2021-07-19 20:02:24 -0700

    hidden django secret key in settings.py

--- Dark Albedo Completed/portfolio/settings.py
+++ Dark Albedo Completed/portfolio/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$t^tep27jj$g9*)+@4!yszi$sk=wx-t6&t^xx41e6o@0ufb6ym'
+SECRET_KEY = 'input secret key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
